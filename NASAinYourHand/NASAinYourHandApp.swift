941b4d856bd2104cf76ee9fb682ef447d89ee68a
Javier Carrillo <jcentercreation@gmail.com>
2022-01-11 17:29:50 +0100

    Initial Commit

---
+++ NASAinYourHand/NASAinYourHandApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAinYourHandApp.swift
+//  NASAinYourHand
+//
+//  Created by Javier Carrillo Gallego on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct NASAinYourHandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
