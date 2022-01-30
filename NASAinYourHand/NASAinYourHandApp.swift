67f3c1b7c37c632467f71f26d5fcd0c8c3d230a8
Javier Carrillo <jcentercreation@gmail.com>
2022-01-30 21:40:18 +0100

    Inicialización de Firebase al iniciarse la app.

--- NASAinYourHand/NASAinYourHandApp.swift
+++ NASAinYourHand/NASAinYourHandApp.swift
@@ -6,9 +6,23 @@
 //
 
 import SwiftUI
+import Foundation
+import Firebase
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct NASAinYourHandApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
