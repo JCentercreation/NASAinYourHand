63a6d98ba6760ebf64018a1375732e44ccde5cce
Javier Carrillo <jcentercreation@gmail.com>
2022-03-12 19:29:42 +0100

    Wecome_View added.

---
+++ NASAinYourHand/Views/Welcome_View.swift
@@ -0,0 +1,20 @@
+//
+//  Welcome_View.swift
+//  NASAinYourHand
+//
+//  Created by Javier Carrillo Gallego on 12/3/22.
+//
+
+import SwiftUI
+
+struct Welcome_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Welcome_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Welcome_View()
+    }
+}
