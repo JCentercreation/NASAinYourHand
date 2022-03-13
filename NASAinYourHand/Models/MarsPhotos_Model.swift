696a835b6b8686b9f40ac89e698540883cbcbb17
Javier Carrillo <jcentercreation@gmail.com>
2022-03-13 22:09:12 +0100

    MarsPhotos_Model implemented.

--- NASAinYourHand/Models/MarsPhotos_Model.swift
+++ NASAinYourHand/Models/MarsPhotos_Model.swift
@@ -9,25 +9,32 @@ import Foundation
 import Combine
 import UIKit
 
-struct InfoMarsPhotos: Hashable {
+struct InfoMarsPhoto: Hashable {
     var id: Int
     var camera_name: String
-    var image: UIImage
+    var image_url: String
+}
+
+enum RoverType: Codable {
+    case curiosity
+    case oportunity
+    case spirit
 }
 
 final class MarsPhotos: ObservableObject {
     
     var subscribersMarsPhotos = Set<AnyCancellable>()
     
-    @Published var infoMarsPhotos: [InfoMarsPhotos]? = []
+    @Published var infoMarsPhotos: [InfoMarsPhoto]? = []
     
-    init(infoMarsPhotos: [InfoMarsPhotos]){
+    init(infoMarsPhotos: [InfoMarsPhoto]){
         self.infoMarsPhotos = infoMarsPhotos
     }
     
-    func getMarsPhotos(date: Date, rover: RoverType){
+    func getMarsPhotos(date: Date, rover: RoverType, completion: @escaping ([InfoMarsPhoto]) -> ()){
         let formatter = DateFormatter()
-        var dateString = formatter.string(from: date)
+        formatter.dateFormat = "yyyy-MM-dd"
+        let dateString = formatter.string(from: date)
         var roverString: String {
             switch rover {
             case .curiosity:
@@ -39,24 +46,85 @@ final class MarsPhotos: ObservableObject {
             }
         }
         let apiKey = "vfC1VzlR2WfH9S8si12Gcb5WmbVA1lVGyZ757HlU"
-        var url = URL(string: "https://api.nasa.gov/mars-photos/api/v1/rovers/"+roverString+"/photos?earth_date="+dateString+"&api_key="+apiKey)
+        let url = URL(string: "https://api.nasa.gov/mars-photos/api/v1/rovers/"+roverString+"/photos?earth_date="+dateString+"&api_key="+apiKey)
+        print(url)
+        
+        struct Camera: Codable {
+            let id: Int?
+            let name: String?
+            let rover_id: Int?
+            let full_name: String?
+        }
+        
+        struct Rover: Codable {
+            let id: Int?
+            let name: String?
+            let landing_date: String?
+            let launch_date: String?
+            let status: String?
+        }
+        
+        struct MarsPhoto: Codable {
+            let id: Int?
+            let sol: Int?
+            let camera: Camera?
+            let img_src: String?
+            let earth_date: String?
+            let rover: Rover?
+        }
+        
+        struct MarsPhotos_GET_response: Codable {
+            let photos: [MarsPhoto]?
+            
+            init(from decoder: Decoder) throws {
+                let container = try decoder.container(keyedBy: CodingKeys.self)
+                let photosDic = try container.decode([MarsPhoto].self, forKey: .photos)
+                self.photos = photosDic
+            }
+        }
+        
+        struct MarsPhotos_GET_request: Codable {
+            let date: Date
+            let rover: RoverType
+            var apiKey: String
+        }
+        
+        if let url = url {
+            let marsPhotosPublisher = URLSession.shared
+                .dataTaskPublisher(for: url)
+                .map(\.data)
+                .decode(type: MarsPhotos_GET_response.self, decoder: JSONDecoder())
+                .compactMap { $0 }
+                .share()
+                .receive(on: RunLoop.main)
+                .eraseToAnyPublisher()
+            
+            marsPhotosPublisher.sink { completion in
+                if case .failure(let error) = completion {
+                    print("Algo ha fallado \(error)")
+                }
+            } receiveValue: { data in
+                data.photos.flatMap { fotos in
+                    fotos.flatMap { foto in
+                        var infoFoto = InfoMarsPhoto(id: foto.id ?? 0, camera_name: foto.camera?.name ?? "", image_url: foto.img_src ?? "")
+                        self.infoMarsPhotos?.append(infoFoto)
+                    }
+                    completion(self.infoMarsPhotos ?? [InfoMarsPhoto(id: 0, camera_name: "", image_url: "")])
+                }
+            }.store(in: &subscribersMarsPhotos)
+
+            
+        }
+        
     }
     
     
-}
-
-enum RoverType: Codable {
-    case curiosity
-    case oportunity
-    case spirit
-}
-
-struct MarsPhotos_GET_request: Codable {
-    let date: Date
-    let rover: RoverType
-    var apiKey: String
-}
-
-struct MarsPhotos_GET_response: Codable {
+    
     
 }
+
+
+
+
+
+
--- NASAinYourHand/Views/MarsPhotos_View.swift
+++ NASAinYourHand/Views/MarsPhotos_View.swift
@@ -8,13 +8,106 @@
 import SwiftUI
 
 struct MarsPhotos_View: View {
+    @StateObject var marsPhotos = MarsPhotos(infoMarsPhotos: [])
+    @State var date: Date = Date.now
+    
+    @State var showDatePicker: Bool = false
+    
+    let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let startComponents = DateComponents(year: 2015, month: 1, day: 1)
+        let nowDate = Date.now
+        let nowCalendar = Calendar.current
+        let nowYear = nowCalendar.component(.year, from: nowDate)
+        let nowMonth = nowCalendar.component(.month, from: nowDate)
+        let nowDay = nowCalendar.component(.day, from: nowDate)
+        let endComponents = DateComponents(year: nowYear, month: nowMonth, day: nowDay)
+        return calendar.date(from:startComponents)!
+            ...
+            calendar.date(from:endComponents)!
+    }()
+    
+    func marsPhotosInfo(date: Date){
+        marsPhotos.infoMarsPhotos?.removeAll()
+        marsPhotos.getMarsPhotos(date: date, rover: .curiosity) { photosArray in
+            for marsPhoto in photosArray {
+                marsPhotos.infoMarsPhotos?.append(marsPhoto)
+            }
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack {
+                Text("Mars Pictures")
+                    .fontWeight(.bold)
+                    .font(Font.title)
+                    .foregroundColor(.gray)
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                Spacer()
+            }
+            HStack {
+                Button {
+                    withAnimation {
+                        showDatePicker.toggle()
+                    }
+                } label: {
+                    HStack {
+                        Text("Date")
+                            .foregroundColor(.black)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Text("\(date.formatted(date: .numeric, time: .omitted))")
+                            .fontWeight(.bold)
+                    }
+                }
+            }.padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                .buttonStyle(.bordered)
+            if showDatePicker == true {
+                DatePicker("Date", selection: $date,in: dateRange, displayedComponents: [.date])
+                    .datePickerStyle(.graphical)
+                    .environment(\.locale, Locale.init(identifier: "en_GB"))
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                    .onChange(of: date) { newValue in
+                        marsPhotosInfo(date: newValue)
+                    }
+            }
+            Spacer()
+            VStack{
+                if marsPhotos.infoMarsPhotos?.isEmpty == false {
+                    List {
+                        ForEach(marsPhotos.infoMarsPhotos ?? [], id: \.self) { foto in
+                            AsyncImage(url: URL(string: foto.image_url), content: { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: 100, maxHeight: 100)
+                            }, placeholder: {
+                                ProgressView()
+                            })
+                        }
+                    }
+                } else {
+                    Spacer()
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .tint(Color.blue)
+                        .scaleEffect(2, anchor: .center)
+                    Spacer()
+                }
+            }
+        }.onAppear {
+            marsPhotosInfo(date: Date.now)
+        }
     }
 }
 
 struct MarsPhotos_View_Previews: PreviewProvider {
     static var previews: some View {
-        MarsPhotos_View()
+        ForEach(["iPhone 13 Pro Max"],id: \.self) { device in
+            MarsPhotos_View()
+                .previewDevice(PreviewDevice(rawValue: device))
+                .previewDisplayName(device)
+        }
     }
 }
