7488f91f2f84b5118d33a57c81f4f4d063233938
Javier Carrillo <jcentercreation@gmail.com>
2022-04-12 23:03:36 +0200

    Introduction_View changes.

--- NASAinYourHand/Views/Introduction_View.swift
+++ NASAinYourHand/Views/Introduction_View.swift
@@ -8,8 +8,78 @@
 import SwiftUI
 
 struct Introduction_View: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Welcome")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            VStack(alignment: .leading) {
+                HStack {
+                    HStack(alignment: .center) {
+                        Image(systemName: "text.below.photo")
+                            .foregroundColor(Color(red: 136 / 255, green: 207 / 255, blue: 139 / 255))
+                            .scaleEffect(2)
+                            .padding()
+                    }.frame(width: 70, height: 70, alignment: .center)
+                    VStack(alignment: .leading) {
+                        Text("Astronomical Picture of the Day")
+                            .font(.callout)
+                            .fontWeight(.bold)
+                        Text("Check the astronomical picture of the day. Learn all science information about the picture and the author.")
+                            .font(.callout)
+                            .foregroundColor(Color.gray)
+                    }
+                }.padding()
+                HStack {
+                    HStack (alignment: .center) {
+                        Image(systemName: "aqi.medium")
+                            .foregroundColor(Color.purple)
+                            .scaleEffect(2)
+                            .padding()
+                    }.frame(width: 70, height: 70, alignment: .center)
+                    VStack(alignment: .leading) {
+                        Text("Asteroids Near Earth")
+                            .font(.callout)
+                            .fontWeight(.bold)
+                        Text("Get to know the asteroids that are surrounding our planet and filter by the most hazardous ones.")
+                            .font(.callout)
+                            .foregroundColor(Color.gray)
+                    }
+                }.padding()
+                HStack {
+                    HStack(alignment: .center) {
+                        Image(systemName: "globe.asia.australia")
+                            .foregroundColor(Color(red: 231 / 255, green: 69 / 255, blue: 53 / 255))
+                            .scaleEffect(2.4)
+                            .padding()
+                    }.frame(width: 70, height: 70, alignment: .center)
+                    VStack(alignment: .leading) {
+                        Text("Mars Rovers Exploration Images")
+                            .font(.callout)
+                            .fontWeight(.bold)
+                        Text("Multiple rovers are exploring the red planet and capturing amazing pictures.")
+                            .font(.callout)
+                            .foregroundColor(Color.gray)
+                    }
+                }.padding()
+            }
+            RoundedRectangle(cornerRadius: 15)
+                .frame(maxHeight: 50)
+                .padding()
+                .overlay(
+                    Text("Continue")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                )
+                .foregroundColor(Color(red: 56 / 255, green: 119 / 255, blue: 237 / 255))
+                .onTapGesture {
+                    presentationMode.wrappedValue.dismiss()
+                }
+        }
     }
 }
 
